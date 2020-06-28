5472c87e3a4178eeabe8d4814b0fe0037a43b8be
Adrian iacob <adrianiacob22@gmail.com>
2020-06-28 12:20:17 +0300

    adaug timpi de asteptare

--- test/pytest/sample.py
+++ test/pytest/sample.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+import time
 import pytest
 import allure
 
